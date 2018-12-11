cd2c0c8f1d4c4de7f797bf45cec02146ff9335dc
Ryan Ehrler <rehrler@global-indemnity.com>
2018-12-11 10:09:22 -0500

    Added ng-rx and company feature module.

---
+++ src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
+
+@NgModule({
+  exports: [MatButtonModule]
+})
+export class AngularMaterialModule {}
---
+++ src/app/core/ngrx/actions/company.actions.ts
@@ -0,0 +1,1 @@
+export const LOAD_COMPANIES = 'LOAD_COMPANIES';
